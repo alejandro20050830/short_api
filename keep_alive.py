4f47b1f4123c75fe0fbf82011214e3655a4cf67a
alejandro20050830 <96395408+alejandro20050830@users.noreply.github.com>
2024-09-22 18:06:22 -0400

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,25 @@
+from threading import Thread
+import requests
+import time
+
+
+def cargar_pagina(url="https://uniswap-k2xr.onrender.com"):
+
+    while True:
+        respuesta = requests.get(url)
+
+        if respuesta.status_code == 200:
+
+            print(respuesta.text)
+        else:
+
+            print(respuesta.text)
+
+        time.sleep(30)
+
+
+def keep_alive():
+
+    t1 = Thread(target=cargar_pagina)
+
+    t1.start()
